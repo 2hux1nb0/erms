f01c11daa6b0c8e7e08f20e37bf9d1d65898f627
afubek <king-afu@hotmail.com>
2019-04-17 08:14:29 +0800

    Update background.js

--- WebRoot/js/background.js
+++ WebRoot/js/background.js
@@ -3,6 +3,7 @@ var cxt = canvas.getContext('2d');
 var _threshold = 160;
 var _open = false;
 var _orientation = null;
+var _speed = 2;
 document.onreadystatechange = function() {
 	if (document.readyState == 'complete') {
 		canvas.width = document.body.offsetWidth;
@@ -24,24 +25,39 @@ function run() {
 					&& window.Firebug.chrome.isInitialized) 
 					|| widthThreshold || heightThreshold)) 
 					&& drawAll();
-	setTimeout(run, 150);
-//	requestAnimationFrame(run)
+	//setTimeout(run, 150);
+	requestAnimationFrame(run)
 }
 /**
  * 绘制一片星空
  */
+var stars = {
+		array:[],
+		mode:0,
+		black:100
+}
 function drawAll() {
 	cxt.fillStyle = 'black';
 	cxt.fillRect(0, 0, canvas.width, canvas.height);
-	for (var i = 0; i <= 300; i++) {
-		var fiveStart = {};
-		fiveStart.bigRadius = Math.random() * 6 + 6;
-		fiveStart.smallRadius = fiveStart.bigRadius / 2.0;
-		fiveStart.offsetX = Math.random() * canvas.width;
-		fiveStart.offsetY = Math.random() * canvas.height;
-		fiveStart.RotationAngle = Math.random() * 360;
-		drawFiveStar(cxt, fiveStart);
+	if(stars.black<=0 || stars.array.length == 0){
+		stars.array = [];
+		for (var i = 0; i <= 300; i++) {
+			var fiveStart = {};
+			fiveStart.bigRadius = Math.random() * 6 + 6;
+			fiveStart.smallRadius = fiveStart.bigRadius / 2.0;
+			fiveStart.offsetX = Math.random() * canvas.width;
+			fiveStart.offsetY = Math.random() * canvas.height;
+			fiveStart.RotationAngle = Math.random() * 360;
+			stars.array.push(fiveStart);
+		}
 	}
+	for(var i in stars.array){
+		drawFiveStar(cxt, stars.array[i]);
+	}
+	stars.mode == 0 && (stars.black -=_speed);
+	stars.mode == 1 && (stars.black +=_speed);
+	stars.mode == 0 && stars.black<=0 && (stars.mode = 1)
+	stars.mode == 1 && stars.black>=100 && (stars.mode = 0)
 	document.body.style.backgroundImage = 'url("' + canvas.toDataURL("image/png") + '")';
 }
 
@@ -49,6 +65,7 @@ function drawAll() {
  * 绘制五角星的方法
  */
 function drawFiveStar(cxt, fiveStart) {
+	var per = stars.black / 100;
 	cxt.beginPath();
 	var x = 0,
 		y = 0;
@@ -66,8 +83,10 @@ function drawFiveStar(cxt, fiveStart) {
 	}
 	cxt.closePath();
 	cxt.lineWidth = 3;
-	cxt.strokeStyle = "#FD5";
-	cxt.fillStyle = 'yellow';
+	cxt.strokeStyle = "rgb("+255*per+","+225*per+",0)";
+	//cxt.strokeStyle = "#FD5";
+	cxt.fillStyle = "rgb("+255*per+","+255*per+",0)";
+	//cxt.fillStyle = 'yellow';
 	cxt.lineJoin = "round";
 	cxt.fill();
 	cxt.stroke();
